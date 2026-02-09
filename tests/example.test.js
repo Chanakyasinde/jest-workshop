da31fba35e46b8d29b38cd3c6013ada96dd442b1
Chanakyasinde <chanakyasinde@gmail.com>
2026-02-09 18:18:10 +0530

    feat: completed all the test cases

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,9 +1,5 @@
 const { calculateFinalAmount } = require("../src/pricing");
 
-test("example: sanity check", () => {
-  expect(1 + 1).toBe(2);
-});
-
 test("Check for invalid sub total", () => {
   expect(() => calculateFinalAmount(-100, "DISCOUNT")).toThrow(
     "Invalid subtotal",
@@ -18,11 +14,12 @@ test("Check for SAVE10 coupon", () => {
   expect(calculateFinalAmount(200, "SAVE10")).toBe(180);
 });
 
+test("Check for FLAT50 boundary case", () => {
+  expect(calculateFinalAmount(50, "FLAT50")).toBe(0);
+});
+
 test("Check for case-insensitive coupon", () => {
   expect(calculateFinalAmount(200, "save10")).toBe(180);
   expect(calculateFinalAmount(200, "SaVe10")).toBe(180);
 });
 
-test("Check for FLAT50 boundary case", () => {
-  expect(calculateFinalAmount(50, "FLAT50")).toBe(0);
-});
